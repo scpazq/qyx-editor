64b1e54e2cfff7fb4703475d9fac5e63acd77d71
zhangqin <valley09@163.com>
2021-09-23 11:10:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-09-23 10:50:54
+ * @LastEditTime: 2021-09-23 11:09:04
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \qyx-editor\src\App.vue
+-->
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld  msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
